7c3264c8a8a419d95db45c1596247b5ea1df2b2a
DavidJsCode <32603727+DavidJsCode@users.noreply.github.com>
2019-10-15 18:13:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+balance = prompt( "What is your bank balance?" );
+phoneNum = prompt("How many phones do you want to buy?");
+// acc = prompt("Do you want accessories?");
+// accCount = prompt("How many accessories do you want?")
+
+const TAX_RATE = .08;
+var amount = 200.00;
